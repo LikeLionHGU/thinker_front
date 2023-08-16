5d639667c9db921e56b05f753897a31c4cc72dc7
ohinhyuk <8156217@naver.com>
2023-08-16 16:45:20 +0900

    feat : 댓글 구현 중 converter를 통해 댓글이 오늘로부터 얼마나 흘렀는 지 감지

---
+++ src/data/CommunityData.ts
@@ -0,0 +1,11 @@
+export const allPost = [
+  {
+    postId: 1,
+    title: 'It is a title',
+    content: 'It is a 이얏! test++++++++++',
+    date: '2023-08-15T03:30:14.06638',
+    member: { memberId: 123, name: 'KimDongGyu', email: 'kdg1941@handong.ac.kr' },
+    postLikeCount: 0,
+    isLiked: false,
+  },
+];
--- src/pages/community/index.tsx
+++ src/pages/community/index.tsx
@@ -1,9 +1,36 @@
-import { Box } from '@mui/material';
+import { Avatar, Box, Typography } from '@mui/material';
 import { useState } from 'react';
+import FlexAlignBox from 'src/components/common/FlexAlignBox';
 import CommentTextArea from 'src/components/community/CommentTextArea';
 import TodayKeywords from 'src/components/home/TodayKeywords';
+import { allPost } from 'src/data/CommunityData';
 
 export default function communityPage() {
+  const converter = (localDateTime) => {
+    const inputDate = new Date(localDateTime);
+    const currentDate = new Date();
+
+    // Calculate the difference in milliseconds
+    const diffInMilliseconds = currentDate - inputDate;
+
+    // Convert time difference into various units
+    const diffInSeconds = Math.floor(diffInMilliseconds / 1000);
+    const diffInMinutes = Math.floor(diffInSeconds / 60);
+    const diffInHours = Math.floor(diffInMinutes / 60);
+    const diffInDays = Math.floor(diffInHours / 24);
+
+    // Determine the appropriate relative string
+    if (diffInDays >= 1) {
+      return `${diffInDays}일전`;
+    } else if (diffInHours >= 1) {
+      return `${diffInHours}시간전`;
+    } else if (diffInMinutes >= 1) {
+      return `${diffInMinutes}분전`;
+    } else {
+      return '방금 전';
+    }
+  };
+
   return (
     <Box
       sx={{
@@ -15,10 +42,55 @@ export default function communityPage() {
         pt: '100px',
       }}
     >
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column', width: '800px' }}>
         <TodayKeywords />
+
         <Box sx={{ p: '30px', borderTop: 1, borderColor: '#303030', mt: '30px' }}>
           <CommentTextArea />
+
+          {allPost.map((post) => (
+            <Box
+              key={post.id}
+              sx={{
+                color: 'white',
+                height: '300px',
+                borderTop: 1,
+                borderColor: '#303030',
+                p: '30px',
+                display: 'flex',
+                justifyContent: 'space-between',
+              }}
+            >
+              <Box
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'space-between',
+                  height: '100%',
+                }}
+              >
+                <Box>
+                  <FlexAlignBox>
+                    <Avatar
+                      sx={{
+                        mr: '20px',
+                        width: '50px',
+                        height: '50px',
+                        backgroundColor: 'success.contrastText',
+                      }}
+                    />
+                    <Typography variant="h6">{post.member.name}</Typography>
+                  </FlexAlignBox>
+                  <Typography variant="body1" sx={{ ml: '70px' }}>
+                    {post.content}
+                  </Typography>
+                </Box>
+
+                <Box></Box>
+              </Box>
+              <Box>{converter(post.date)}</Box>
+            </Box>
+          ))}
         </Box>
       </Box>
       <Box sx={{ width: '300px', height: '350px', border: 1 }}>달력</Box>
