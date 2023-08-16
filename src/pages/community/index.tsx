fc86165823ef27ae20e33bd4b229d1bdc4354439
ohinhyuk <8156217@naver.com>
2023-08-16 16:20:52 +0900

    style : 댓글창 + 버튼 구현

--- src/pages/community/index.tsx
+++ src/pages/community/index.tsx
@@ -1,6 +1,27 @@
-export default function communityPage(){ 
-    
-    return (
-        
-    );
+import { Box } from '@mui/material';
+import { useState } from 'react';
+import CommentTextArea from 'src/components/community/CommentTextArea';
+import TodayKeywords from 'src/components/home/TodayKeywords';
+
+export default function communityPage() {
+  return (
+    <Box
+      sx={{
+        backgroundColor: 'success.darker',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        gap: '50px',
+        pt: '100px',
+      }}
+    >
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+        <TodayKeywords />
+        <Box sx={{ p: '30px', borderTop: 1, borderColor: '#303030', mt: '30px' }}>
+          <CommentTextArea />
+        </Box>
+      </Box>
+      <Box sx={{ width: '300px', height: '350px', border: 1 }}>달력</Box>
+    </Box>
+  );
 }
