dc1ca2aa557389c4bc0c5df781620d3da6487a11
ohinhyuk <8156217@naver.com>
2023-08-16 14:26:18 +0900

    style : 서비스 모아보기 ui 만들기

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -3,20 +3,72 @@ import Sidebar from '../../components/profile/Sidebar';
 import Image from 'next/image';
 import FlexAlignBox from '../../components/common/FlexAlignBox';
 import EmptyAlert from 'src/components/profile/EmptyAlert';
+import SearchResultBlock from 'src/components/idea/SearchResultBlock';
+import { member } from './data';
+
+type SearchResultBlockProps = {
+  item: {
+    title: string;
+    link: string;
+    snippet: string;
+    pagemap: {
+      metatags: {
+        'og:image': string;
+      }[];
+    };
+  };
+};
+
 export default function profile() {
+  const converter = (data): SearchResultBlockProps => {
+    const item = {
+      title: data.title,
+      snippet: '',
+      link: data.url,
+      pagemap: {
+        metatags: [
+          {
+            'og:image': '/assets/images/profile/profileLock.png',
+          },
+        ],
+      },
+      // image: '/assets/images/home/logo_thinkey.png',
+    };
+    return item;
+  };
   return (
     <Box
       sx={{
         minHeight: '100vh',
         backgroundColor: 'success.darker',
         display: 'flex',
-        alignItems: 'center',
+
         justifyContent: 'center',
-        pt: '100px',
+        pt: '132px',
       }}
     >
       <Sidebar />
-      <EmptyAlert />
+      {/* <EmptyAlert /> */}
+      <Box sx={{ width: '1100px', display: 'flex', flexDirection: 'column', gap: '20px' }}>
+        <Box
+          sx={{
+            backgroundColor: '#6665F6',
+            borderRadius: '10px',
+            height: '60px',
+            color: 'success.contrastText',
+            fontSize: '20px',
+            display: 'flex',
+            alignItems: 'center',
+            pl: '20px',
+            mb: '20px',
+          }}
+        >
+          서비스 모아보기
+        </Box>
+        {member.bookMarkList.map((item, index) => (
+          <SearchResultBlock key={index} item={converter(item)} />
+        ))}
+      </Box>
     </Box>
   );
 }
